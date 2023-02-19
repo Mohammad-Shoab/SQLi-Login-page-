a96237e419d795eb53f7d9a54d4c96c3d50bdfb0
Mohammad-Shoab <88820736+Mohammad-Shoab@users.noreply.github.com>
2023-02-19 10:36:12 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostName = "sql12.freemysqlhosting.net";
-$dbUser = "sql12595260";
-$dbPassword = "4fhvidZGGl";
-$dbName = "sql12595260";
+$hostName = "";
+$dbUser = "";
+$dbPassword = "";
+$dbName = "";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
